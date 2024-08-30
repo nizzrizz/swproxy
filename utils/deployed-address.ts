a2266f45f302c71c0b0bc6f63f475385c5073c36
nizzrizz <100416456+nizzrizz@users.noreply.github.com>
2024-08-30 19:17:39 +0000

    feat: initiated the project

---
+++ utils/deployed-address.ts
@@ -0,0 +1,4 @@
+export const SWTRProxy = '0xf34F50c5c5b75c3766A0c688fbba5b44CB0376aE'
+export const ProxyAdmin = '0x8011E96DB0bCD6C38AC553c8Ef6eFd1cd525aAC7'
+export const SWTRImplementation = '0xb298AeF81Ddbc83472a99B3D5eF36b70b93d6368'
+    
